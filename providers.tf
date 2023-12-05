b31d7e3df73d52821d7ea208ded209ae4fffd279
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2023-12-05 16:16:37 +0000

    PUB-2265 - Dependency updates

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {}
 
-  required_version = ">= 1.0.4"
+  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
-      version = ">=2.0.0"
+      version = "3.83.0"
     }
   }
 }
